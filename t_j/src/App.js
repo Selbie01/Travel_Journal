1550ccdd661659d1299e3f94614bad633e5655c1
Selbie01 <sawyerselby@gmail.com>
2022-08-06 17:36:40 -0500

    finished body, project complete

--- t_j/src/App.js
+++ t_j/src/App.js
@@ -3,15 +3,16 @@ import Header from "./Header"
 import bodyData from "./BodyData"
 import Body from "./Body"
 function App() {
-  /*const body = bodyData.map(items => return {
+  const body = bodyData.map(items => {
     return ( <Body 
       key = {items.id}
       {...items}
       />)
-  })*/
+  })
   return (
     <>
       <Header />
+      {body}
     </>
   );
 }
--- t_j/src/Body.js
+++ t_j/src/Body.js
@@ -1,6 +1,19 @@
 import React from 'react'
 
 export default function Body(props){
-    
-
+    return (
+    <div className = "card">
+        <img src = {props.image} alt = "" className="card-image"/>
+        <div className='card-details'>
+            <span className= "country">
+                <img src = {`./imgs/${props.decor}`} alt = "" className = "decor"/>
+                {props.country}
+                <span className="link"> {props.link}</span>
+            </span>
+            <span className = "location">{props.location}</span>
+            <span className='date'>{props.date}</span>
+            <span className = "description">{props.description}</span>
+       </div>
+       </div>
+    );
 }
